782c1014030e3bfd2f87f27052c8415ccb05324d
ashevchenko <aleksey.sevcenco@bequant.io>
2023-11-20 22:26:33 +0200

    group anagrams

--- group-anagrams/group-anagrams.go
+++ group-anagrams/group-anagrams.go
@@ -1,5 +1,51 @@
 package main
 
-func groupAnagrams(strs []string) [][]string {
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
+func groupAnagrams(strs []string) [][]string {
+	m := make(map[string][]string)
+
+	for _, str := range strs {
+		runeToCountMap := make(map[rune]int)
+
+		for _, v := range str {
+			_, found := runeToCountMap[v]
+
+			if found {
+				runeToCountMap[v]++
+			} else {
+				runeToCountMap[v] = 1
+			}
+		}
+
+		resultKey := make([]string, 0)
+
+		for k, v := range runeToCountMap {
+			resultKey = append(resultKey, string(k)+fmt.Sprint(v))
+		}
+
+		sort.Strings(resultKey)
+
+		resultKeyStr := strings.Join(resultKey, ":")
+
+		_, found := m[resultKeyStr]
+
+		if found {
+			m[resultKeyStr] = append(m[resultKeyStr], str)
+		} else {
+			m[resultKeyStr] = []string{str}
+		}
+	}
+
+	var result [][]string
+
+	for _, v := range m {
+		result = append(result, v)
+	}
+
+	return result
 }
--- group-anagrams/group-anagrams_test.go
+++ group-anagrams/group-anagrams_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestProblem(t *testing.T) {
+	require.Equal(t, groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"}), [][]string{
+		[]string{"bat"},
+		[]string{"nat", "tan"},
+		[]string{"ate", "eat", "tea"},
+	})
+}
