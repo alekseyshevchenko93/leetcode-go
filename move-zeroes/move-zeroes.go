17ed7e47577399267f30ae00def1bfbaf343944c
ashevchenko <aleksey.sevcenco@bequant.io>
2023-11-21 20:28:05 +0200

    move zeroes

---
+++ move-zeroes/move-zeroes.go
@@ -0,0 +1,21 @@
+package main
+
+func moveZeroes(nums []int) {
+	s := 0
+
+	for i, v := range nums {
+		if v != 0 {
+			if i != s {
+				temp := nums[i]
+				nums[i] = nums[s]
+				nums[s] = temp
+			}
+
+			s++
+		}
+
+		if v == 0 {
+			continue
+		}
+	}
+}
---
+++ move-zeroes/move-zeroes_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestProblem(t *testing.T) {
+	tt := []struct {
+		input []int
+		outpt []int
+	}{
+		{
+			[]int{0, 1, 0, 3, 12},
+			[]int{1, 3, 12, 0, 0},
+		},
+		{
+			[]int{0, 0, 1, 1},
+			[]int{1, 1, 0, 0},
+		},
+		{
+			[]int{0, 1, 0, 1},
+			[]int{1, 1, 0, 0},
+		},
+		{
+			[]int{0, 1, 0, 0, 0, 0, 1},
+			[]int{1, 1, 0, 0, 0, 0, 0},
+		},
+		{
+			[]int{1, 2},
+			[]int{1, 2},
+		},
+		{
+			[]int{0, 1, 0, 3, 12},
+			[]int{1, 3, 12, 0, 0},
+		},
+	}
+
+	for _, v := range tt {
+		moveZeroes(v.input)
+		require.Equal(t, v.outpt, v.input)
+	}
+}
