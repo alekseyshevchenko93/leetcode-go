c8c0d2bf1cadbcd53ab4b7e8e5ceeb9507b6d8a2
ashevchenko <aleksey.sevcenco@bequant.io>
2023-11-30 23:32:18 +0200

    binary-tree-paths

--- problems/binary-tree-paths/binary-tree-paths.go
+++ problems/binary-tree-paths/binary-tree-paths.go
@@ -10,31 +10,36 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func helper(root *TreeNode, current string, results []string) []string {
-	if root == nil {
-		return results
-	}
-
-	if root.Left == nil && root.Right == nil {
-		return append(results, current)
-	}
-
-	if root.Left != nil {
-		results = helper(root.Left, fmt.Sprintf("%s->%s", current, fmt.Sprint(root.Left.Val)), results)
-	}
-
-	if root.Right != nil {
-		results = helper(root.Right, fmt.Sprintf("%s->%s", current, fmt.Sprint(root.Right.Val)), results)
-	}
-
-	return results
-}
-
 func binaryTreePaths(root *TreeNode) []string {
 	if root == nil {
 		return []string{}
 	}
 
 	results := make([]string, 0)
-	return helper(root, fmt.Sprint(root.Val), results)
+	var helper func(*TreeNode, string)
+
+	helper = func(node *TreeNode, path string) {
+		if node == nil {
+			return
+		}
+
+		if node.Left == nil && node.Right == nil {
+			results = append(results, path)
+			return
+		}
+
+		if node.Left != nil {
+			helper(node.Left, fmt.Sprintf("%s->%s", path, fmt.Sprint(node.Left.Val)))
+		}
+
+		if node.Right != nil {
+			helper(node.Right, fmt.Sprintf("%s->%s", path, fmt.Sprint(node.Right.Val)))
+		}
+	}
+
+	helper(root, fmt.Sprint(root.Val))
+
+	fmt.Println(results)
+
+	return results
 }
